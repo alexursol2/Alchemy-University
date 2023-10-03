5439fa0d65641b5e9429e2e686c642319bc0d884
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2023-10-03 18:56:53 +0200

    Delete week1 directory

--- week1/hashmessage.js
+++
@@ -1,8 +0,0 @@
-const { keccak256 } = require("ethereum-cryptography/keccak");
-const { utf8ToBytes } = require("ethereum-cryptography/utils");
-
-function hashMessage(message) {
-    return keccak256(utf8ToBytes(message));
-}
-
-module.exports = hashMessage;
